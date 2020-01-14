42678a60a02383107475ed406a0776991cbd3e46
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-01-14 14:15:27 -0600

    followers rendered to screen

--- reactgithub/src/App.js
+++ reactgithub/src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: []
+      user: [],
+      followers: []
     }
   }
 
@@ -21,13 +22,22 @@ class App extends React.Component {
       .catch((err) => {
         console.log(`There was an error: ${err}`);
       })
+
+    axios.get(`https://api.github.com/users/alex-lc/followers`)
+      .then((res) => {
+        // console.log(res);
+        this.setState({ followers: res.data })
+      })
+      .catch((err) => {
+        console.log(`There was an error ${err}`);
+      })
   }
 
   render() {
     return (
       <div>
         {/* {console.log(this.state.user)} */}
-        <UserCard user={this.state.user} />
+        <UserCard user={this.state.user} followers={this.state.followers} />
       </div>
     )
   }
--- reactgithub/src/components/UserCard.js
+++ reactgithub/src/components/UserCard.js
@@ -2,27 +2,60 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
 
+    .user-info {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+
+        .avatar {
+            width: 20%;
+    
+            img {
+                width: 100%;
+            }
+        }
+    
+        .info {
+            width: 75%;
+        }
+    }
 `;
 
 export default function UserCard(props) {
 
-    const { user } = props;
+    const { user, followers } = props;
 
     return (
         <Card>
-            <div className="avatar">
-                <img src={user.avatar_url} alt="User Avatar" />
+            <div className="user-info">
+                <div className="avatar">
+                    <img src={user.avatar_url} alt="User Avatar" />
+                </div>
+                <div className="info">
+                    <p>Name: {user.name}</p>
+                    <p>Username: <a href={user.html_url}>{user.login}</a></p>
+                    <p>Company: {user.company}</p>
+                    <p>Website: <a href={user.blog}>{user.blog}</a></p>
+                    <p>Location: {user.location}</p>
+                    <p>Followers: {user.followers}</p>
+                    <p>Following: {user.following}</p>
+                    <p>Bio: {user.bio}</p>
+                </div>
             </div>
-            <div className="info">
-                <p>Name: {user.name}</p>
-                <p>Username: <a href={user.html_url}>{user.login}</a></p>
-                <p>Company: {user.company}</p>
-                <p>Website: <a href={user.blog}>{user.blog}</a></p>
-                <p>Location: {user.location}</p>
-                <p>Followers: {user.followers}</p>
-                <p>Following: {user.following}</p>
-                <p>Bio: {user.bio}</p>
+
+            <div className="followers">
+                {
+                    followers.map((follower) => {
+                        return <p>{follower.login}</p>
+                    })
+                }
             </div>
         </Card>
     )
